e6d9a4ad6119164118dbeb8ede51e28444d2db45
kadjialan <alankadji7@gmail.com>
2022-12-15 15:31:48 +0100

    linters resolution

--- asserts/script/script.js
+++ asserts/script/script.js
@@ -1,17 +1,17 @@
 const buttons = document.querySelectorAll('.bleuk')
-let bt1 = document.getElementById('1')
-let bt2 = document.getElementById('2')
-let bt3 = document.getElementById('3')
-let bt4 = document.getElementById('4')
-let bt5 = document.getElementById('5')
-let bt6 = document.getElementById('6')
-let bt7 = document.getElementById('7')
-let bt8 = document.getElementById('8')
-let bt9 = document.getElementById('9')
+const bt1 = document.getElementById('1')
+const bt2 = document.getElementById('2')
+const bt3 = document.getElementById('3')
+const bt4 = document.getElementById('4')
+const bt5 = document.getElementById('5')
+const bt6 = document.getElementById('6')
+const bt7 = document.getElementById('7')
+const bt8 = document.getElementById('8')
+const bt9 = document.getElementById('9')
 let changeValue
-let x = [1,2,3,6,9,8,7,4]
+const x = [1, 2 , 3 , 6 , 9 , 8 , 7 , 4]
 
-bt5.addEventListener('click', function() { 
+bt5.addEventListener ('click', function () {
     clockWise()
     rename()
 })
@@ -23,7 +23,6 @@ for (const i of buttons) {
     })
 }
 
-
  function clockWise() {
         changeValue = x.pop()
         x.unshift(changeValue)
@@ -31,7 +30,6 @@ for (const i of buttons) {
 
 
 function rename() {
-
     bt1.innerHTML = x[0]
     bt2.innerHTML = x[1]
     bt3.innerHTML = x[2]
@@ -39,9 +37,7 @@ function rename() {
     bt6.innerHTML = x[3]
     bt7.innerHTML = x[6]
     bt8.innerHTML = x[5]
-    bt9.innerHTML = x[4]
-
-    
+    bt9.innerHTML = x[4]  
 }
 
 function antiClockWise() {
