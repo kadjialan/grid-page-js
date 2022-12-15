df7d86db476393d980129e39e25e905dd610e4d7
kadjialan <alankadji7@gmail.com>
2022-12-15 15:42:25 +0100

    linters resolution

--- asserts/script/script.js
+++ asserts/script/script.js
@@ -9,40 +9,38 @@ const bt7 = document.getElementById('7')
 const bt8 = document.getElementById('8')
 const bt9 = document.getElementById('9')
 let changeValue
-const x = [1, 2 , 3 , 6 , 9 , 8 , 7 , 4]
+const x = [1, 2, 3, 6, 9, 8, 7, 4]
 
 bt5.addEventListener ('click', function () {
-    clockWise()
-    rename()
+  clockWise()
+  rename()
 })
 
 for (const i of buttons) {
     i.addEventListener('click', function() { 
-        antiClockWise()
-        rename()
-    })
+    antiClockWise()
+    rename()
+  })
 }
 
- function clockWise() {
-        changeValue = x.pop()
-        x.unshift(changeValue)
+function clockWise() {
+  changeValue = x.pop()
+  x.unshift(changeValue)
 }
-
 
 function rename() {
-    bt1.innerHTML = x[0]
-    bt2.innerHTML = x[1]
-    bt3.innerHTML = x[2]
-    bt4.innerHTML = x[7]
-    bt6.innerHTML = x[3]
-    bt7.innerHTML = x[6]
-    bt8.innerHTML = x[5]
-    bt9.innerHTML = x[4]  
+  bt1.innerHTML = x[0]
+  bt2.innerHTML = x[1]
+  bt3.innerHTML = x[2]
+  bt4.innerHTML = x[7]
+  bt6.innerHTML = x[3]
+  bt7.innerHTML = x[6]
+  bt8.innerHTML = x[5]
+  bt9.innerHTML = x[4]
 }
 
 function antiClockWise() {
-    changeValue = x.shift()
-        x.push(changeValue)
+  changeValue = x.shift()
+  x.push(changeValue)
 }
-
 
